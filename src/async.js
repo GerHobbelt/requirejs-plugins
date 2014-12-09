11deaa0b353aed5fe61c50713dc38d5245180fe0
Attila Beregszaszi <attila@frontseed.com>
2014-12-09 16:09:19 +0000

    Added support for a global callback name

--- src/async.js
+++ src/async.js
@@ -6,39 +6,55 @@
  */
 define(function(){
 
-    var DEFAULT_PARAM_NAME = 'callback',
-        _uid = 0;
+  var DEFAULT_PARAM_NAME = 'callback',
+    _uid = 0;
 
-    function injectScript(src){
-        var s, t;
-        s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = src;
-        t = document.getElementsByTagName('script')[0]; t.parentNode.insertBefore(s,t);
+  function injectScript(src){
+    var s, t;
+    s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = src;
+    t = document.getElementsByTagName('script')[0]; t.parentNode.insertBefore(s,t);
+  }
+
+  function formatUrl(name, id){
+    var paramRegex = /!(.+)/,
+        url = name.replace(paramRegex, ''),
+        param = (paramRegex.test(name)) ? name.replace(/.+!/, '') : DEFAULT_PARAM_NAME;
+
+    url += (url.indexOf('?') < 0)? '?' : '&';
+
+    return url + param +'='+ id;
+  }
+
+  function parse(name) {
+    var parts = name.split(':');
+
+    return parts[1];
+  }
+
+  function uid(name) {
+    var glob = parse(name);
+
+    if (glob === undefined) {
+      _uid += 1;
+      return '__async_req_'+ _uid +'__';
     }
-
-    function formatUrl(name, id){
-        var paramRegex = /!(.+)/,
-            url = name.replace(paramRegex, ''),
-            param = (paramRegex.test(name))? name.replace(/.+!/, '') : DEFAULT_PARAM_NAME;
-        url += (url.indexOf('?') < 0)? '?' : '&';
-        return url + param +'='+ id;
+    else {
+      return glob;
     }
+  }
 
-    function uid() {
-        _uid += 1;
-        return '__async_req_'+ _uid +'__';
+  return {
+    load: function(name, req, onLoad, config){
+      if (config.isBuild) {
+        onLoad(null); // Do not do anything on builds.
+      }
+      else {
+        var id = uid(name);
+        // Create a global variable that stores onLoad so callback function can
+        // define new module after async load.
+        window[id] = onLoad;
+        injectScript(formatUrl(req.toUrl(name), id));
+      }
     }
-
-    return{
-        load : function(name, req, onLoad, config){
-            if(config.isBuild){
-                onLoad(null); //avoid errors on the optimizer
-            }else{
-                var id = uid();
-                //create a global variable that stores onLoad so callback
-                //function can define new module after async load
-                window[id] = onLoad;
-                injectScript(formatUrl(req.toUrl(name), id));
-            }
-        }
-    };
+  };
 });
